d1a2af9761b2368e6790e9b90b395347cdb42d3d
Boris Kosoric <boris.kosoric.etf@gmail.com>
2022-12-12 22:42:38 +0100

    promeni u big decimal

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,53 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class Main {
 
     public static void main(String[] args) {
-        double vrednostStana = 231360;
-        double ucesceProcenat = 20;
-        double ucesceSuma = vrednostStana * ucesceProcenat / 100;
-        double preostaliDug = vrednostStana * (100 - ucesceProcenat) / 100;
-        final double iznosKredita = preostaliDug;
-        double kamatnaStopa = 7.55;
-        int brojGodinaOtplate = 20;
-        int mesecnaDoplata = 0;
+        BigDecimal vrednostStana = new BigDecimal(231360);
+        BigDecimal ucesceProcenat = new BigDecimal(20);
+        BigDecimal ucesceSuma = vrednostStana.multiply(ucesceProcenat).divide(new BigDecimal(100), MathContext.DECIMAL128);
+        BigDecimal preostaliDug = vrednostStana.multiply(new BigDecimal(100).subtract(ucesceProcenat)).divide(new BigDecimal(100), MathContext.DECIMAL128);
+        BigDecimal iznosKredita = new BigDecimal(String.valueOf(preostaliDug));
+        BigDecimal kamatnaStopa = new BigDecimal("7.55");
+        BigDecimal brojGodinaOtplate = new BigDecimal(20);
+        BigDecimal mesecnaDoplata = new BigDecimal(0);
 
-        double mesecnaRata = izracunajMesecnuRatu(preostaliDug, kamatnaStopa, brojGodinaOtplate);
+        BigDecimal mesecnaRata = izracunajMesecnuRatu(preostaliDug, kamatnaStopa, brojGodinaOtplate);
 
-        System.out.println("Vrednost stana: " + (int) vrednostStana);
-        System.out.println("Ucesce: " + (int) ucesceSuma);
-        System.out.println("Iznos kredita: " + (int) preostaliDug);
+        System.out.println("Vrednost stana: " + vrednostStana.toBigInteger());
+        System.out.println("Ucesce: " + ucesceSuma.toBigInteger());
+        System.out.println("Iznos kredita: " + preostaliDug.toBigInteger());
         System.out.println("Kamatna Stopa: " + kamatnaStopa);
         System.out.println("Godine otplate: " + brojGodinaOtplate);
-        System.out.println("Mesecna rata + mesecna doplata = " + (int) mesecnaRata + " + " + mesecnaDoplata);
+        System.out.println("Mesecna rata + mesecna doplata = " + mesecnaRata.toBigInteger() + " + " + mesecnaDoplata);
 
-        double ukupanTrosakKredita = 0;
+        BigDecimal ukupanTrosakKredita = new BigDecimal(0);
 
-        for (int trenutnaGodina = 1; trenutnaGodina <= (brojGodinaOtplate+1); trenutnaGodina++) {
+        for (int trenutnaGodina = 1; trenutnaGodina <= (brojGodinaOtplate.intValue() + 1); trenutnaGodina++) {
 
-            double godisnjaRata = 12 * mesecnaRata;
-            double godisnjaKamata = preostaliDug * kamatnaStopa / 100;
-            double godisnjaDoplata = 12 * mesecnaDoplata;
+            BigDecimal godisnjaRata = mesecnaRata.multiply(new BigDecimal(12));
+            BigDecimal godisnjaKamata = preostaliDug.multiply(kamatnaStopa).divide(new BigDecimal(100), MathContext.DECIMAL128);
+            BigDecimal godisnjaDoplata = mesecnaDoplata.multiply(new BigDecimal(12));
 
-            preostaliDug = preostaliDug + godisnjaKamata - godisnjaRata - godisnjaDoplata;
-            System.out.println("Dugovanje na kraju " + trenutnaGodina + ".\t godine\t= " + (int) preostaliDug);
-            ukupanTrosakKredita += godisnjaRata + godisnjaDoplata;
+            preostaliDug = preostaliDug.add(godisnjaKamata).subtract(godisnjaRata).subtract(godisnjaDoplata);
+            System.out.println("Dugovanje na kraju " + trenutnaGodina + ".\t godine\t= " + preostaliDug.intValue());
+            ukupanTrosakKredita = ukupanTrosakKredita.add(godisnjaRata).add(godisnjaDoplata);
 
-            if (preostaliDug < 0) {
-                ukupanTrosakKredita = ukupanTrosakKredita + preostaliDug;
-                System.out.println("Ukupan trosak kredita: " + (int) ukupanTrosakKredita);
-                System.out.println("Banci poklonili: " + (int) (ukupanTrosakKredita - iznosKredita));
+            if (preostaliDug.compareTo(new BigDecimal(0)) < 0) {
+                ukupanTrosakKredita = ukupanTrosakKredita.add(preostaliDug);
+                System.out.println("Ukupan trosak kredita: " + ukupanTrosakKredita.intValue());
+                System.out.println("Banci poklonili: " + (ukupanTrosakKredita.subtract(iznosKredita)).intValue());
                 break;
             }
         }
     }
 
-    private static double izracunajMesecnuRatu(double iznosKredita, double godisnjaKamatnaStopa, int brojGodinaOtplate) {
-        double mesecnaKamatnaStopa = godisnjaKamatnaStopa / 100 / 12;
-        int ukupanBrojRata = brojGodinaOtplate * 12;
-        double ukupnaEfektivnaKamatnaStopa = Math.pow(1 + mesecnaKamatnaStopa, ukupanBrojRata);
-        return iznosKredita * (mesecnaKamatnaStopa * ukupnaEfektivnaKamatnaStopa / (ukupnaEfektivnaKamatnaStopa - 1));
+    private static BigDecimal izracunajMesecnuRatu(BigDecimal iznosKredita, BigDecimal godisnjaKamatnaStopa, BigDecimal brojGodinaOtplate) {
+        BigDecimal mesecnaKamatnaStopa = (godisnjaKamatnaStopa.divide(new BigDecimal(100), MathContext.DECIMAL128)).divide(new BigDecimal(12), MathContext.DECIMAL128);
+        BigDecimal ukupanBrojRata = brojGodinaOtplate.multiply(new BigDecimal(12));
+        BigDecimal ukupnaEfektivnaKamatnaStopa = (mesecnaKamatnaStopa.add(new BigDecimal(1))).pow(ukupanBrojRata.intValue());
+        return iznosKredita.multiply(mesecnaKamatnaStopa.multiply(ukupnaEfektivnaKamatnaStopa).divide(ukupnaEfektivnaKamatnaStopa.subtract(new BigDecimal("1")), MathContext.DECIMAL128));
     }
 
 }
